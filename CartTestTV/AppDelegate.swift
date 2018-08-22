ed814a931f05bfcea3b0d096db94ecf8f69e36f3
Martin Bing <bing.martin.photography@gmail.com>
2018-08-22 18:06:53 +0200

    Add watch and tv to tests

--- CartTestTV/AppDelegate.swift
+++ CartTestTV/AppDelegate.swift
@@ -1,37 +1,27 @@
 //
 //  AppDelegate.swift
-//  CartTest
+//  CartTestTV
 //
-//  Created by Martin Demiddel on 26.03.18.
+//  Created by Martin Demiddel on 22.08.18.
 //  Copyright © 2018 Martin Demiddel. All rights reserved.
 //
 
 import UIKit
-import Webtrekk
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
+
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        try! WebtrekkTracking.initTrack()
-        let certificates: [Data] = {
-            let url = Bundle.main.url(forResource: "webtrekknet", withExtension: "cer")!
-            let data = try! Data(contentsOf: url)
-
-            return [data]
-        }()
-
-        WebtrekkTracking.setPinning(certificates: certificates)
-
         return true
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
